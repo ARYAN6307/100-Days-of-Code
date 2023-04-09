9da89987705afbfb5b48b5df2898e3a807905b3c
Aryan Pandey <90707959+ARYAN6307@users.noreply.github.com>
2023-04-09 20:32:40 +0530

    Time: 721 ms (5.01%), Space: 43.5 MB (23.06%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int trap(int[] arr) {
+        int ans=0;
+
+        for(int i=0;i<arr.length;i++){
+            int left =0;
+            for(int j=0;j<=i;j++){
+                left =Math.max(left,arr[j]);
+            }
+            int right = Integer.MIN_VALUE;
+            for(int j=i;j<=arr.length-1;j++){
+                right = Math.max(right,arr[j]);
+            }
+            ans += Math.min(left,right)-arr[i];
+        }
+
+        return ans;
+    }
+}
